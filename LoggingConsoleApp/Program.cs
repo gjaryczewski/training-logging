678efa5d5295b4730d4d63ebf3f8820684fce746
Gerard Jaryczewski <gerard.jaryczewski@shijigroup.com>
2020-07-22 20:19:19 +0200

    Initial commit

---
+++ LoggingConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.EventLog;
+
+namespace LoggingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logSettings = new EventLogSettings
+            {
+                Filter = (source, level) => level >= LogLevel.Trace
+            };
+
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter("Microsoft", LogLevel.Warning)
+                    .AddFilter("System", LogLevel.Warning)
+                    .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+                    .AddConsole()
+                    .AddEventLog(logSettings);
+            });
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+            logger.LogInformation(1234, "Example log information message");
+            logger.LogWarning(5678, "Example log warning message");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # training-logging
+
+https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-3.1
+https://github.com/dotnet/AspNetCore.Docs/tree/master/aspnetcore/fundamentals/logging/index/samples/3.x
